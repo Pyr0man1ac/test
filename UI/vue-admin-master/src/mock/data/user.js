2955cba5f72c147deea6aa91a7ce4d833f9724c1
Aiden <MoronSlayer@outlook.com>
2018-09-12 13:34:07 +0800

    password

--- UI/vue-admin-master/src/mock/data/user.js
+++ UI/vue-admin-master/src/mock/data/user.js
@@ -3,7 +3,7 @@ const LoginUsers = [
   {
     id: 1,
     username: 'admin',
-    password: '000000',
+    password: '000',
     avatar: 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png',
     name: '摸鱼'
   }
